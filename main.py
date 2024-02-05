e85ac6a9e7965442e54fd50db5b6202e19e7cb7a
lucky shedrack <lucky.shedrack@binghamuni.edu.ng>
2024-02-06 00:02:41 +0100

    cos

---
+++ main.py
@@ -0,0 +1,18 @@
+m = []
+
+def chocks():
+    d = [' ','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm']
+    k = ['EMPTY SPACE','Har Tomdoo Paradise', 'Tyavzua Joy Iember', 'Ulaga Benedict Ulaga', 'Gabriel Flourish', 'David Samuel Ngbede',
+         'Iorchivir Mark Tavershima', 'Sunday Divine', 'Anderson Wisdom Chinemerem', 'Akoso Pelumi David',
+         'Onah Love Ene', 'Adoga Gideon', 'Desmond', 'Akoso David Keghtor']
+
+
+
+    code = input('Enter Your Code: ')
+
+
+
+while True:
+    chocks()
+
+
